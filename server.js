baa0ac6ddffd1b33713c2b6e92c1f1898d21b638
Shalini Rawat <113cs0170@nitrkl.ac.in>
2016-09-05 13:25:10 +0530

    express files added

--- server.js
+++ server.js
@@ -1,64 +1,18 @@
-//this file has different libraries
-//express library
+//for using express it has all the methods like get ,post, put, delete
 var express = require('express');
-
-//morgan library whenever a user goes to specific url it(url) will show in cmd
-var morgan = require('morgan');
-
-var mongoose = require('mongoose');
-
-//it take the body of req and parse it to server and what we want it to recieve
-var bodyParser = require('body-parser');
-
-var ejs = require('ejs');
-
-//we need this to create webpages
-var engine = require('ejs-mate');
-
-// to use user.js file 
-var User = require('./models/user');
 
 //app is referring to express objects
 var app = express();
 
-mongoose.connect('mongodb://root:abc123@ds019906.mlab.com:19906/ecommerce_site', function(err){
-	if (err){
-		console.log(err);
-
-	}
-	else{
-		console.log("Connected to database");
-	}
+//it tells the server to response something, get method 
+app.get('/name', function(req,res){
+	res.json("My name is Shalini");
 });
 
-//middleware to invoke morgan objects
-
-//for use of static files in public folder
-app.use(express.static(__dirname+'/public'));
-
-app.use(morgan('dev'))
-
-//middleware to invoke bodyparser objects
-app.use(bodyParser.json());
-
-//to parse
-app.use(bodyParser.urlencoded({ extended: true}));
-
-app.engine('ejs', engine);
-
-app.set('view engine', 'ejs');
-
-//it is a user defined app
-var mainRoutes = require('./routes/main');
-
-app.use(mainRoutes);
-
-var userRoutes = require('./routes/user');
-
-app.use(userRoutes);
-
-//express methods listen is used, to run the app, to know wjhether its running or error is there
-app.listen(8000, function(err){
-	if (err) throw err;
-	console.log("Server is running on port 8000");
+//sending something to server, post method
+/*app.post()-to send data to server,app.delete(), app.put()-updating data
+*/
+app.listen(3000, function(err){
+	if(err) throw err;
+	console.log("Server is running");
 });
